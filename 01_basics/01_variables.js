a78de9e491c4fe088bffc2085214c00730b031da
Subhadeep Mandal <145041243+mandal722@users.noreply.github.com>
2026-03-14 10:53:22 +0000

    learn variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const account=12
+let accountemail="s@gmail"
+var accountPass = "12345" //not use for scope problem
+accountCity = "kolkata"
+let accountState;
+console.log(account)
+//constant dont change
+accountemail="man@gmail"
+
+console.table([accountemail,accountPass,accountCity,accountState]);
